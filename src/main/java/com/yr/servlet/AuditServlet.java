cf5f682f28e6c6d51d373bcabae935d6ba2a364a
Linshuiqiaolj <34386824+Linshuiqiaolj@users.noreply.github.com>
2018-01-19 09:42:14 +0800

    项目
    项目

--- src/main/java/com/yr/servlet/AuditServlet.java
+++ src/main/java/com/yr/servlet/AuditServlet.java
@@ -1,6 +1,15 @@
 package com.yr.servlet;
 
+import com.yr.dao.SellerDao;
+import com.yr.pojo.Seller;
+import com.yr.util.SellerPage;
+import net.sf.json.JSONObject;
+
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,7 +46,38 @@ public class AuditServlet extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        response.setContentType("text/json");
+        response.setCharacterEncoding("utf-8");
+
+        //页面显示值
+        PrintWriter out = response.getWriter();
+        String type= request.getParameter("type");
+        String select= request.getParameter("select");//搜索功能中输入框的值(账号)
+        String pageNow = request.getParameter("pageNow");//获得页面传过来的当前页
+
+        if (null != type && type.equals("list")) {
+            String sel = request.getParameter("select");
+            if (null == pageNow || "".equals(pageNow)) {
+                pageNow = "1";
+            }
+            // 查询并分页
+            List<Seller> list = SellerDao.selectGoods(select,Integer.valueOf(pageNow),sel);
+            // 获得总页数
+            int pageCount=SellerDao.getPageCount();
+            // 显示当前页
+            String pageCode = new SellerPage().getPageCode(Integer.parseInt(pageNow), pageCount);
+            Map<String, Object> map = new HashMap<>();
+            map.put("list", list);
+            map.put("pageCount", pageCount + "");
+            map.put("pageNow", pageNow);
+            map.put("pageCode", pageCode);
+            String jsonObjectStr = JSONObject.fromObject(map).toString();
+            out.write(jsonObjectStr);
+            out.flush();
+            out.close();
+        }
+
     }
 
 }
+
